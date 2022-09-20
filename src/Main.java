4258d1ed18c6d829389fe25ef3098772a6f1b06f
hoffbaaj <hoffbaaj@mail.uc.edu>
2022-09-20 12:38:03 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int age;
+
+        System.out.println("What is your age?);
+        // Simulate the input code which we learn later
+
+        age=18;
+
+        if(age < 15)
+        {
+            System.out.println("You can enter the Children's Cave.");
+        }
+    }
+}
